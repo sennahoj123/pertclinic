8be2d48e191958af7a0820101ded60d1269664d1
iede <iede.adu@devoteam.com>
2024-05-24 12:51:04 +0200

    new

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -106,8 +106,8 @@ resource "azurerm_linux_virtual_machine" "az_vm" {
 
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
     version   = "latest"
   }
 }
