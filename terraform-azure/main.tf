770b21e255c7a27b5a672f9853e8e1a0363e8024
iede <iede.adu@devoteam.com>
2024-05-31 22:58:49 +0200

    k

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -96,7 +96,7 @@ resource "azurerm_linux_virtual_machine" "az_vm" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("/home/adminuser/.ssh/id_rsa.pub")
   }
 
   os_disk {
