3428886affa5a9b5d902a83b3c69bb8dcf72dc97
iede <iede.adu@devoteam.com>
2024-05-31 13:54:17 +0200

    9

--- terraform-azure/generate_inventory.sh
+++ terraform-azure/generate_inventory.sh
@@ -1,16 +1,17 @@
+#!/bin/bash
+
 # Get the public IP addresses from Terraform output
-PUBLIC_IPS=$(terraform output public_ip_address)
+PUBLIC_IPS=$(cat $1 | jq -r '.public_ip_address.value[]')
 
 # Write the Ansible inventory file
 echo "[own_pc]
 localhost ansible_connection=local ansible_user=iede
 
-echo "[Testing]"
-echo "$PUBLIC_IPS" | awk '{print $1, "ansible_user=adminuser ansible_ssh_private_key_file=~/.ssh/id_rsa.pub"}'
+[Testing]"
+echo "$PUBLIC_IPS" | awk 'NR==1 {print $1, "ansible_user=adminuser ansible_ssh_private_key_file=~/.ssh/id_rsa.pub"}'
 
 echo "[Acceptance]"
-echo "$PUBLIC_IPS" | awk '{print $2, "ansible_user=adminuser ansible_ssh_private_key_file=~/.ssh/id_rsa.pub"}'
+echo "$PUBLIC_IPS" | awk 'NR==2 {print $1, "ansible_user=adminuser ansible_ssh_private_key_file=~/.ssh/id_rsa.pub"}'
 
 echo "[Production]"
-echo "$PUBLIC_IPS" | awk '{print $3, "ansible_user=adminuser ansible_ssh_private_key_file=~/.ssh/id_rsa.pub"}'
-
+echo "$PUBLIC_IPS" | awk 'NR==3 {print $1, "ansible_user=adminuser ansible_ssh_private_key_file=~/.ssh/id_rsa.pub"}'
--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -111,6 +111,120 @@ resource "azurerm_linux_virtual_machine" "az_vm" {
     version   = "latest"
   }
 }
-output "public_ip_address" {
-  value = values(azurerm_public_ip.az_ip)[*].ip_address
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
 }
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_resource_group" "existing" {
+  name = "iede_adu-rg"
+}
+
+resource "azurerm_virtual_network" "az_vn" {
+  name                = "iede_adu-rg-vnet"
+  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
+  address_space       = ["10.123.0.0/16"]
+}
+
+resource "azurerm_subnet" "az_sn" {
+  name                 = "iede_adu-rg-subnet"
+  resource_group_name  = data.azurerm_resource_group.existing.name
+  virtual_network_name = azurerm_virtual_network.az_vn.name
+  address_prefixes     = ["10.123.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "az_sg" {
+  name                = "iede_adu-rg-security"
+  location            = data.azurerm_resource_group.existing.location
+  resource_group_name = data.azurerm_resource_group.existing.name
+}
+
+resource "azurerm_network_security_rule" "az_sr" {
+  name                        = "iede_adu-rg-rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = data.azurerm_resource_group.existing.name
+  network_security_group_name = azurerm_network_security_group.az_sg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "az_sn" {
+  subnet_id                 = azurerm_subnet.az_sn.id
+  network_security_group_id = azurerm_network_security_group.az_sg.id
+}
+
+resource "azurerm_public_ip" "az_ip" {
+  for_each = var.vm_map
+
+  name                = "${each.value.name}-ip"
+  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "dev"
+  }
+}
+
+resource "azurerm_network_interface" "az_ni" {
+  for_each            = var.vm_map
+
+  name                = "${each.value.name}-ni"
+  location            = data.azurerm_resource_group.existing.location
+  resource_group_name = data.azurerm_resource_group.existing.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.az_sn.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.az_ip[each.key].id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "az_vm" {
+  for_each = var.vm_map
+
+  name                = each.value.name
+  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
+  size                = "Standard_B2s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.az_ni[each.key].id
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+}
+output "public_ip_addresses" {
+  value = [for ip in azurerm_public_ip.az_ip : ip.ip_address]
+}
+
