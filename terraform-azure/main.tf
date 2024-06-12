3ce202a701c6c2f30061e9286c8d45ae588f164a
iede <iede.adu@devoteam.com>
2024-06-12 12:39:18 +0200

    update main.tf

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -95,7 +95,7 @@ resource "azurerm_linux_virtual_machine" "az_vm" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("${path.module}/public_key.pem")
+    public_key = file("/home/adminuser/.ssh/id_rsa.pub")
   }
 
   os_disk {
