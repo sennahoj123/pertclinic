4b436ebcb469a71a6fe24fc58779bf8c908cf144
iede <iede.adu@devoteam.com>
2024-06-26 17:08:41 +0200

    gggg

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -23,19 +23,25 @@ data "azurerm_subnet" "internal" {
 
 resource "azurerm_linux_virtual_machine" "vm1" {
   name                = "VM1"
-  resource_group_name = data.azurerm_resource_group.existing.name
-  location            = data.azurerm_resource_group.existing.location
+  resource_group_name = azurerm_resource_group.existing.name
+  location            = azurerm_resource_group.existing.location
   size                = "Standard_B2s"
   admin_username      = "adminuser"
+
   admin_ssh_key {
     username   = "adminuser"
     public_key = file("~/.ssh/id_rsa.pub")
   }
-  network_interface_ids = [azurerm_network_interface.vm1-nic.id]
+
+  network_interface_ids = [
+    azurerm_network_interface.vm1-nic.id
+  ]
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
+
   source_image_reference {
     publisher = "Canonical"
     offer     = "ubuntu-24_04-lts"
@@ -44,21 +50,27 @@ resource "azurerm_linux_virtual_machine" "vm1" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "vm2" {
-  name                = "VM2"
-  resource_group_name = data.azurerm_resource_group.existing.name
-  location            = data.azurerm_resource_group.existing.location
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = "VM1"
+  resource_group_name = azurerm_resource_group.existing.name
+  location            = azurerm_resource_group.existing.location
   size                = "Standard_B2s"
   admin_username      = "adminuser"
+
   admin_ssh_key {
     username   = "adminuser"
     public_key = file("~/.ssh/id_rsa.pub")
   }
-  network_interface_ids = [azurerm_network_interface.vm2-nic.id]
+
+  network_interface_ids = [
+    azurerm_network_interface.vm1-nic.id
+  ]
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
+
   source_image_reference {
     publisher = "Canonical"
     offer     = "ubuntu-24_04-lts"
@@ -67,21 +79,27 @@ resource "azurerm_linux_virtual_machine" "vm2" {
   }
 }
 
-resource "azurerm_linux_virtual_machine" "production" {
-  name                = "Production"
-  resource_group_name = data.azurerm_resource_group.existing.name
-  location            = data.azurerm_resource_group.existing.location
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = "VM1"
+  resource_group_name = azurerm_resource_group.existing.name
+  location            = azurerm_resource_group.existing.location
   size                = "Standard_B2s"
   admin_username      = "adminuser"
+
   admin_ssh_key {
     username   = "adminuser"
     public_key = file("~/.ssh/id_rsa.pub")
   }
-  network_interface_ids = [azurerm_network_interface.production-nic.id]
+
+  network_interface_ids = [
+    azurerm_network_interface.vm1-nic.id
+  ]
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
+
   source_image_reference {
     publisher = "Canonical"
     offer     = "ubuntu-24_04-lts"
