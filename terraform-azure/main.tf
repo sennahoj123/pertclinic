a080988fbc32b8c782eee1dc7c7895ae2f398f78
iede <iede.adu@devoteam.com>
2024-06-26 21:21:38 +0200

    ddssdf

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -21,19 +21,64 @@ data "azurerm_subnet" "internal" {
   resource_group_name  = data.azurerm_resource_group.existing.name
 }
 
-data "azurerm_network_interface" "vm1-nic" {
+resource "azurerm_public_ip" "vm1_public_ip" {
+  name                = "vm1PublicIP"
+  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_public_ip" "vm2_public_ip" {
+  name                = "vm2PublicIP"
+  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_public_ip" "production_public_ip" {
+  name                = "productionPublicIP"
+  resource_group_name = data.azurerm_resource_group.existing.name
+  location            = data.azurerm_resource_group.existing.location
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_network_interface" "vm1-nic" {
   name                = "vm1-nic"
+  location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = data.azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm1_public_ip.id
+  }
 }
 
-data "azurerm_network_interface" "vm2-nic" {
+resource "azurerm_network_interface" "vm2-nic" {
   name                = "vm2-nic"
+  location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = data.azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vm2_public_ip.id
+  }
 }
 
-data "azurerm_network_interface" "production-nic" {
+resource "azurerm_network_interface" "production-nic" {
   name                = "production-nic"
+  location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = data.azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.production_public_ip.id
+  }
 }
 
 resource "azurerm_linux_virtual_machine" "vm1" {
@@ -49,7 +94,7 @@ resource "azurerm_linux_virtual_machine" "vm1" {
   }
 
   network_interface_ids = [
-    data.azurerm_network_interface.vm1-nic.id
+    azurerm_network_interface.vm1-nic.id
   ]
 
   os_disk {
@@ -78,7 +123,7 @@ resource "azurerm_linux_virtual_machine" "vm2" {
   }
 
   network_interface_ids = [
-    data.azurerm_network_interface.vm2-nic.id
+    azurerm_network_interface.vm2-nic.id
   ]
 
   os_disk {
@@ -107,7 +152,7 @@ resource "azurerm_linux_virtual_machine" "production" {
   }
 
   network_interface_ids = [
-    data.azurerm_network_interface.production-nic.id
+    azurerm_network_interface.production-nic.id
   ]
 
   os_disk {
@@ -125,8 +170,8 @@ resource "azurerm_linux_virtual_machine" "production" {
 
 output "vm_public_ips" {
   value = {
-    VM1        = azurerm_linux_virtual_machine.vm1.public_ip_address
-    VM2        = azurerm_linux_virtual_machine.vm2.public_ip_address
-    Production = azurerm_linux_virtual_machine.production.public_ip_address
+    VM1        = azurerm_public_ip.vm1_public_ip.ip_address
+    VM2        = azurerm_public_ip.vm2_public_ip.ip_address
+    Production = azurerm_public_ip.production_public_ip.ip_address
   }
 }
