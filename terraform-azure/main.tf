34f5e9db433597cf66f89efafef68bc474b1a3d9
iede <iede.adu@devoteam.com>
2024-06-26 12:27:43 +0200

    pl

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -18,20 +18,17 @@ data "azurerm_resource_group" "existing" {
 data "azurerm_virtual_network" "existing" {
   name                = "iede_adu-rg-vnet"
   resource_group_name = data.azurerm_resource_group.existing.name
-  location            = data.azurerm_resource_group.existing.location
-  address_space       = ["10.123.0.0/16"]
 }
 
 resource "azurerm_subnet" "az_sn" {
   name                 = "iede_adu-rg-subnet"
   resource_group_name  = data.azurerm_resource_group.existing.name
-  virtual_network_name = azurerm_virtual_network.az_vn.name
+  virtual_network_name = data.azurerm_virtual_network.existing.name
   address_prefixes     = ["10.123.1.0/24"]
 }
 
 data "azurerm_network_security_group" "existing" {
   name                = "iede_adu-rg-security"
-  location            = data.azurerm_resource_group.existing.location
   resource_group_name = data.azurerm_resource_group.existing.name
 }
 
@@ -46,7 +43,7 @@ resource "azurerm_network_security_rule" "az_sr" {
   source_address_prefix       = "*"
   destination_address_prefix  = "*"
   resource_group_name         = data.azurerm_resource_group.existing.name
-  network_security_group_name = azurerm_network_security_group.az_sg.name
+  network_security_group_name = data.azurerm_network_security_group.existing.name
 }
 
 resource "azurerm_subnet_network_security_group_association" "az_sn" {
