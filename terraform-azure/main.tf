e6de937c98c1bbb838345b20ac24bc946942a32d
iede <iede.adu@devoteam.com>
2024-06-26 16:07:30 +0200

    new terraform

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -1,90 +1,94 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-}
+
 
 provider "azurerm" {
   features {}
 }
 
-data "azurerm_resource_group" "existing" {
-  name = "iede_adu-rg"
+resource "azurerm_resource_group" "existing" {
+  name     = "iede_adu-rg"
+  location = "West Europe"  # Replace with the actual location of your resource group
 }
 
-data "azurerm_virtual_network" "existing" {
-  name                = "iede_adu-rg-vnet"
-  resource_group_name = data.azurerm_resource_group.existing.name
-}
-
-data "azurerm_subnet" "az_sn" {
-  name                 = "iede_adu-rg-subnet"
-  virtual_network_name = data.azurerm_virtual_network.existing.name
-  resource_group_name  = data.azurerm_resource_group.existing.name
-}
-
-data "azurerm_network_security_group" "existing" {
-  name                = "iede_adu-rg-security"
-  resource_group_name = data.azurerm_resource_group.existing.name
-}
-
-data "azurerm_public_ip" "existing" {
-  for_each = var.vm_map
-
-  name                = "${each.key}-ip"
-  resource_group_name = data.azurerm_resource_group.existing.name
-}
-
-resource "azurerm_network_interface" "az_ni" {
-  for_each            = var.vm_map
-
-  name                = "${each.key}-ni"
-  location            = data.azurerm_resource_group.existing.location
-  resource_group_name = data.azurerm_resource_group.existing.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = data.azurerm_subnet.az_sn.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = data.azurerm_public_ip.existing[each.key].id
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "az_vm" {
-  for_each = var.vm_map
-
-  name                = each.value.name
-  resource_group_name = data.azurerm_resource_group.existing.name
-  location            = data.azurerm_resource_group.existing.location
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = "VM1"
+  resource_group_name = azurerm_resource_group.existing.name
+  location            = azurerm_resource_group.existing.location
   size                = "Standard_B2s"
-  admin_username      = "adminuser"
-  network_interface_ids = [
-    azurerm_network_interface.az_ni[each.key].id
-  ]
-
+  
+  admin_username      = "adminuser"  # Replace with your desired admin username
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("/home/adminuser/.ssh/id_rsa.pub")
+    public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your SSH public key
   }
-
+  
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
-
+  
   source_image_reference {
     publisher = "Canonical"
-    offer     = "ubuntu-24_04-lts"
-    sku       = "server"
+    offer     = "UbuntuServer"
+    sku       = "20_04-lts"
     version   = "latest"
   }
 }
 
-output "public_ip_addresses" {
-  value = {
-    for key, public_ip in data.azurerm_public_ip.existing : key => public_ip.ip_address
+resource "azurerm_linux_virtual_machine" "vm2" {
+  name                = "VM2"
+  resource_group_name = azurerm_resource_group.existing.name
+  location            = azurerm_resource_group.existing.location
+  size                = "Standard_B2s"
+  
+  admin_username      = "adminuser"  # Replace with your desired admin username
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your SSH public key
   }
+  
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "production" {
+  name                = "Production"
+  resource_group_name = azurerm_resource_group.existing.name
+  location            = azurerm_resource_group.existing.location
+  size                = "Standard_B2s"
+  
+  admin_username      = "adminuser"  # Replace with your desired admin username
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your SSH public key
+  }
+  
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+}
+
+output "vm_public_ips" {
+  value = {
+    "VM1"        = azurerm_linux_virtual_machine.vm1.public_ip_address
+    "VM2"        = azurerm_linux_virtual_machine.vm2.public_ip_address
+    "Production" = azurerm_linux_virtual_machine.production.public_ip_address
+  }
+  json = true
 }
