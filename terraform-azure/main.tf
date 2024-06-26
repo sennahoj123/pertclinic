2fe33e3cd58fabe0818d3cc4a4dc617cdf00d1bf
iede <iede.adu@devoteam.com>
2024-06-26 16:43:07 +0200

    wd

--- terraform-azure/main.tf
+++ terraform-azure/main.tf
@@ -11,59 +11,15 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "existing" {
+data "azurerm_resource_group" "existing" {
   name     = "iede_adu-rg"
   location = "West Europe"  # Replace with the actual location of your resource group
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.existing.location
-  resource_group_name = azurerm_resource_group.existing.name
-}
-
-resource "azurerm_subnet" "internal" {
+data "azurerm_subnet" "internal" {
   name                 = "internal"
+  virtual_network_name = "example-network"  # Replace with your existing virtual network name
   resource_group_name  = azurerm_resource_group.existing.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.2.0/24"]
-}
-
-resource "azurerm_network_interface" "vm1-nic" {
-  name                      = "vm1-nic"
-  location                  = azurerm_resource_group.existing.location
-  resource_group_name       = azurerm_resource_group.existing.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_network_interface" "vm2-nic" {
-  name                      = "vm2-nic"
-  location                  = azurerm_resource_group.existing.location
-  resource_group_name       = azurerm_resource_group.existing.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_network_interface" "production-nic" {
-  name                      = "production-nic"
-  location                  = azurerm_resource_group.existing.location
-  resource_group_name       = azurerm_resource_group.existing.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.internal.id
-    private_ip_address_allocation = "Dynamic"
-  }
 }
 
 resource "azurerm_linux_virtual_machine" "vm1" {
